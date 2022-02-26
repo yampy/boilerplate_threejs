bffe78f68963aead699335cecaa3882c381efe7a
yampy <gstyley@gmail.com>
2022-02-26 14:52:37 +0900

    initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Boilerplate for Three.js web app
+
+## Install
+```
+npm i
+```
+
+## Build
+```
+npm run build
+```
+
+## Run
+```
+npm start
+```
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { hello } from './hello';
+import * as THREE from 'three';
+
+// Setup Renderer 
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.querySelector('.webGLContainer').appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+// Setup Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 10);
+camera.lookAt(0, 0, 0);
+
+// Add Sample Geometry
+const boxGeometry = new THREE.BoxGeometry();
+const normalMaterial = new THREE.MeshNormalMaterial;
+const boxMesh = new THREE.Mesh(boxGeometry, normalMaterial);
+scene.add(boxMesh);
+
+// Render & Animation
+function render() {
+    boxMesh.rotation.x += 0.01;
+    boxMesh.rotation.y += 0.01;
+    boxMesh.rotation.z += 0.01;
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+
+render();
